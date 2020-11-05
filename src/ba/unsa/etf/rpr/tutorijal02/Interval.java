e24a0a0839aba023cae9a8688e4b769486d76b60
Haris <hkukuruzov1@etf.unsa.ba>
2020-11-06 00:21:30 +0100

    Popravljene sve metode osim preklapanja jednako za objekte.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -45,9 +45,63 @@ public boolean isNull(){
     return false;
     }
     public Interval intersect(Interval v1){
-        return new Interval(Double.min(v1.getPocetna(), this.getKrajnja()),Double.min(v1.getKrajnja(),this.getKrajnja()),true,true);
+        if(this.getPocetna()<v1.getPocetna()){
+            if(this.getKrajnja()<v1.getPocetna())
+                return new Interval(0,0,false,false);
+        }
+        else{
+            if(v1.getKrajnja()<this.getPocetna())
+                return new Interval(0,0,false,false);
+        }
+        double a=Double.max(v1.getPocetna(), this.getPocetna());
+        double b=Double.min(v1.getKrajnja(), this.getKrajnja());
+        boolean c,d;
+        if(v1.getPocetna()==a)
+            c=v1.isPocetniInterval();
+        else
+            c=this.isPocetniInterval();
+        if(v1.getKrajnja()==b)
+            d=v1.isKrajnjiInterval();
+        else
+            d=this.isKrajnjiInterval();
+        return new Interval(a, b,c,d);
     }
     public static Interval intersect(Interval v1,Interval v2){
-        return new Interval(Double.min(v1.getPocetna(), v2.getKrajnja()),Double.min(v1.getKrajnja(),v2.getKrajnja()),true,true);
+        if(v2.getPocetna()<v1.getPocetna()){
+            if(v2.getKrajnja()<v1.getPocetna())
+                return new Interval(0,0,false,false);
+        }
+        else{
+            if(v1.getKrajnja()<v2.getPocetna())
+                return new Interval(0,0,false,false);
+        }
+        double a=Double.max(v1.getPocetna(), v2.getPocetna());
+        double b=Double.min(v1.getKrajnja(), v2.getKrajnja());
+        boolean c,d;
+        if(v1.getPocetna()==a)
+            c=v1.isPocetniInterval();
+        else
+            c=v2.isPocetniInterval();
+        if(v1.getKrajnja()==b)
+            d=v1.isKrajnjiInterval();
+        else
+            d=v2.isKrajnjiInterval();
+        return new Interval(a,b,c,d);
+    }
+    public String toString(){
+        if(this.isNull())
+            return "("+")";
+        else if(this.isKrajnjiInterval() && this.isPocetniInterval())
+            return "["+this.getPocetna()+","+this.getKrajnja()+"]";
+        else if(this.isKrajnjiInterval() && this.isPocetniInterval()==false)
+            return "("+this.getPocetna()+","+this.getKrajnja()+"]";
+            else if(this.isKrajnjiInterval()==false && this.isPocetniInterval())
+            return "["+this.getPocetna()+","+this.getKrajnja()+")";
+        return "("+this.getPocetna()+","+this.getKrajnja()+")";
+
+    }
+    public boolean equals( Interval f){
+        return this.getPocetna()==f.getPocetna() && this.getKrajnja()==f.getKrajnja() &&
+                f.isKrajnjiInterval()==this.isKrajnjiInterval() && f.isPocetniInterval()==this.isPocetniInterval();
     }
 }
